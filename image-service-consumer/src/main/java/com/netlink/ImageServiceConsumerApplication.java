05554aeb6067c1c3660a0d3be9b9855706e36b9e
fubencheng <fubencheng@zhongan.com>
2018-06-06 21:16:14 +0800

    添加代码

--- image-service-consumer/src/main/java/com/netlink/ImageServiceConsumerApplication.java
+++ image-service-consumer/src/main/java/com/netlink/ImageServiceConsumerApplication.java
@@ -2,6 +2,11 @@ package com.netlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * ImageServiceConsumerApplication
@@ -9,9 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author fubencheng
  * @version 0.0.1 2018-05-15 10:44 fubencheng
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ImageServiceConsumerApplication {
     public static void main( String[] args ) {
         SpringApplication.run(ImageServiceConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Do any additional configuration here
+        return builder.build();
     }
 }
--- image-service-provider-beta/src/main/java/com/netlink/ImageServiceProviderBetaApplication.java
+++ image-service-provider-beta/src/main/java/com/netlink/ImageServiceProviderBetaApplication.java
@@ -2,13 +2,15 @@ package com.netlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * ImageServiceProviderApplication
+ * ImageServiceProviderBetaApplication
  *
  * @author fubencheng
  * @version 0.0.1 2018-05-15 10:44 fubencheng
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ImageServiceProviderBetaApplication {
     public static void main(String[] args) {
